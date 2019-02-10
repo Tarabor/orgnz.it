9668bca9888d79be989097ba42fccc80fc7a713a
Marco Taraborrelli <marco.taraborrelli@gmail.com>
2019-02-10 20:38:33 +0100

    addEvent page translated

--- frontend/src/app/members/addEvent/addEvent.module.ts
+++ frontend/src/app/members/addEvent/addEvent.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,6 +14,7 @@ import { AddEventPage } from './addEvent.page';
     FormsModule,
     ReactiveFormsModule,
     IonicModule,
+    TranslateModule.forChild(),
     RouterModule.forChild([
       {
         path: '',
