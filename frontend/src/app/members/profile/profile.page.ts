3d5950afef0315ca29c2fcf98444eb14e6960682
Marco Taraborrelli <marco.taraborrelli@gmail.com>
2019-04-14 18:52:58 +0200

    added profile and events to dashboard

---
+++ frontend/src/app/members/profile/profile.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
+})
+export class ProfilePage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
---
+++ frontend/src/app/members/profile/profle.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ProfilePage } from './profile.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ProfilePage
+      }
+    ])
+  ],
+  declarations: [ProfilePage]
+})
+export class ProfilePageModule {}
